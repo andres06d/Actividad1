014a5d1834702f80de9c85beb0f3838e4e0d57b9
andres <andres.vinos@gmail.com>
2023-05-03 21:03:45 -0500

    cambio nombre

--- c_genero.php
+++ c_genero.php
@@ -12,7 +12,7 @@
 
 //con query
 try {
-    $mbd = new PDO('mysql:host=localhost;dbname=desarrollo_web', 'r0ot', '');
+    $mbd = new PDO('mysql:host=localhost;dbname=desarrollo_web', 'root', '');
 
    
     //consultas preparadas 
